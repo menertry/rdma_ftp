1f220400cc4e89cd1d4a25186c6c174e80556e34
dyinnz <ml_143@sina.com>
2015-07-19 22:25:38 +0800

    .

--- dyinnz/server.c
+++ dyinnz/server.c
@@ -17,15 +17,22 @@ char    recv_msg[MAXLEN] = "recive test!";
 
 /******************************************************************************/
 
-void get_connect_request(struct rdma_cm_id *listen_id) {
-    struct rdma_cm_id   *id = NULL;
-    struct ibv_mr       *mr = NULL;
+void get_connect_request(struct rdma_cm_id *id) {
+    struct ibv_mr           *mr = NULL;
+    struct ibv_qp_init_attr attr;
 
-    if (0 != rdma_get_request(listen_id, &id)) {
-        perror("rmda_get_request");
+    memset(&attr, 0, sizeof(attr));
+    attr.cap.max_send_wr = attr.cap.max_recv_wr = 1;
+    attr.cap.max_send_sge = attr.cap.max_recv_sge = 1;
+    attr.cap.max_inline_data = MAXLEN;
+    attr.sq_sig_all = 1;
+
+    // TODO: release it
+    if (0 != rdma_create_qp(id, NULL, &attr)) {
+        perror("rdma_create_qp");
         return;
     }
-    
+
     if ( !(mr = rdma_reg_msgs(id, recv_msg, MAXLEN)) ) {
         perror("rdma_reg_msgs");
         return;
@@ -72,11 +79,7 @@ void rdma_event_handle(int fd, short lib_event, void *arg) {
 }
 
 int main(int argc, char *argv[]) {
-    struct ibv_qp_init_attr     qp_init_attr;
-
-    struct rdma_addrinfo        hints,
-                                *res = NULL;
-
+    struct sockaddr_in          addr;
     struct rdma_cm_id           *listen_id = NULL;
 
     struct event_base           *base = NULL;
@@ -84,48 +87,24 @@ int main(int argc, char *argv[]) {
 
     uint16_t                    port = 0;
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_flags = RAI_PASSIVE;
-	hints.ai_port_space = RDMA_PS_TCP;
-	if (0 != rdma_getaddrinfo(NULL, NULL, &hints, &res)) {
-        perror("rdma_getaddrinfo");
-        return -1;
-	}
-
-	memset(&qp_init_attr, 0, sizeof(qp_init_attr));
-	qp_init_attr.cap.max_send_wr = qp_init_attr.cap.max_recv_wr = 1;
-	qp_init_attr.cap.max_send_sge = qp_init_attr.cap.max_recv_sge = 1;
-	qp_init_attr.cap.max_inline_data = 16;
-	qp_init_attr.sq_sig_all = 1;
-
-    if (0 != rdma_create_ep(&listen_id, res, NULL, &qp_init_attr)) {
-        perror("rdma_create_ep");
-        return -1;
-    }
-	rdma_freeaddrinfo(res);
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
 
     if ( !(channel = rdma_create_event_channel()) ) {
         perror("rdma_create_event_channel");
         return -1;
     }
 
-    if (0 != rdma_migrate_id(listen_id, channel)) {
-        perror("rdma_migrate_id");
-        return -1;
-    }
-
-/*
     if (0 != rdma_create_id(channel, &listen_id, NULL, RDMA_PS_TCP) )  {
         perror("rdma_create_id");
         return -1;
     }
 
-
     if (0 != rdma_bind_addr(listen_id, (struct sockaddr *)&addr)) {
         perror("rdma_bind_addr");
         return -1;
     }
-*/
+
     if (0 != rdma_listen(listen_id, 10)) {
         perror("rdma_listen");
         return -1;
@@ -136,13 +115,12 @@ int main(int argc, char *argv[]) {
     printf("Listening on port %d\n", port);
 
     base = event_base_new();
-    event_listen = calloc(1, sizeof(struct event));
+    event_listen = calloc(1, sizeof(event_listen));
 
     // TODO
     event_set(event_listen, channel->fd, EV_READ | EV_PERSIST, rdma_event_handle, NULL);
     event_base_set(base, event_listen);
     event_add(event_listen, NULL);
-
     event_base_dispatch(base);
 
     // Release resources 
