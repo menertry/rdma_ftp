cf04ad3254aab42e7c6982708e84c5eec5a121ed
dyinnz <ml_143@sina.com>
2015-07-19 22:02:04 +0800

    server.c

--- dyinnz/server.c
+++ dyinnz/server.c
@@ -72,7 +72,11 @@ void rdma_event_handle(int fd, short lib_event, void *arg) {
 }
 
 int main(int argc, char *argv[]) {
-    struct sockaddr_in          addr;
+    struct ibv_qp_init_attr     qp_init_attr;
+
+    struct rdma_addrinfo        hints,
+                                *res = NULL;
+
     struct rdma_cm_id           *listen_id = NULL;
 
     struct event_base           *base = NULL;
@@ -80,24 +84,48 @@ int main(int argc, char *argv[]) {
 
     uint16_t                    port = 0;
 
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_flags = RAI_PASSIVE;
+	hints.ai_port_space = RDMA_PS_TCP;
+	if (0 != rdma_getaddrinfo(NULL, NULL, &hints, &res)) {
+        perror("rdma_getaddrinfo");
+        return -1;
+	}
+
+	memset(&qp_init_attr, 0, sizeof(qp_init_attr));
+	qp_init_attr.cap.max_send_wr = qp_init_attr.cap.max_recv_wr = 1;
+	qp_init_attr.cap.max_send_sge = qp_init_attr.cap.max_recv_sge = 1;
+	qp_init_attr.cap.max_inline_data = 16;
+	qp_init_attr.sq_sig_all = 1;
+
+    if (0 != rdma_create_ep(&listen_id, res, NULL, &qp_init_attr)) {
+        perror("rdma_create_ep");
+        return -1;
+    }
+	rdma_freeaddrinfo(res);
 
     if ( !(channel = rdma_create_event_channel()) ) {
         perror("rdma_create_event_channel");
         return -1;
     }
 
+    if (0 != rdma_migrate_id(listen_id, channel)) {
+        perror("rdma_migrate_id");
+        return -1;
+    }
+
+/*
     if (0 != rdma_create_id(channel, &listen_id, NULL, RDMA_PS_TCP) )  {
         perror("rdma_create_id");
         return -1;
     }
 
+
     if (0 != rdma_bind_addr(listen_id, (struct sockaddr *)&addr)) {
         perror("rdma_bind_addr");
         return -1;
     }
-
+*/
     if (0 != rdma_listen(listen_id, 10)) {
         perror("rdma_listen");
         return -1;
@@ -108,12 +136,13 @@ int main(int argc, char *argv[]) {
     printf("Listening on port %d\n", port);
 
     base = event_base_new();
-    event_listen = calloc(1, sizeof(event_listen));
+    event_listen = calloc(1, sizeof(struct event));
 
     // TODO
     event_set(event_listen, channel->fd, EV_READ | EV_PERSIST, rdma_event_handle, NULL);
     event_base_set(base, event_listen);
     event_add(event_listen, NULL);
+
     event_base_dispatch(base);
 
     // Release resources 
